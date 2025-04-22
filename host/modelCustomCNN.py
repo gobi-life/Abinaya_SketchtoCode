f1f2a0c6cfc3172b336ea2f527b5e07c696c716d
gobi-life <gobi@lifefitnessindia.com>
2025-04-22 15:02:19 +0530

    first commit

---
+++ host/modelCustomCNN.py
@@ -0,0 +1,101 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+
+class CustomCNN(nn.Module):
+    def __init__(self, output_size):
+        super(CustomCNN, self).__init__()
+        
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=8, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(in_channels=8, out_channels=16, kernel_size=3, stride=1, padding=1)
+        self.conv3 = nn.Conv2d(in_channels=16, out_channels=8, kernel_size=3, stride=1, padding=1)
+        
+        self._initialize_weights()
+
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
+        
+        self.dropout1 = nn.Dropout(0.15)  
+        self.dropout2 = nn.Dropout(0.25)  
+        self.dropout3 = nn.Dropout(0.35)  
+        
+        self.activation = nn.LeakyReLU(negative_slope=0.01)
+        
+        self.fc1 = nn.Linear(in_features=8 * 28 * 28, out_features=512)
+        self.fc2 = nn.Linear(in_features=512, out_features=output_size)
+
+    def forward(self, x):
+        
+        x = self.dropout1(self.pool(self.activation(self.conv1(x)))) # (8, 112, 112)
+        x = self.dropout2(self.pool(self.activation(self.conv2(x)))) # (16, 56, 56)
+        x = self.dropout3(self.pool(self.activation(self.conv3(x)))) # (8, 28, 28)
+        
+        x = x.view(x.size(0), -1)
+        
+        x = self.activation(self.fc1(x))
+        x = self.fc2(x)
+        return x
+    
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='leaky_relu')
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, 0, 0.01)
+                nn.init.constant_(m.bias, 0)
+
+
+class Encoder(nn.Module):
+    def __init__(self, embedding_size):
+        super(Encoder, self).__init__()
+        self.custom_cnn = CustomCNN(output_size=embedding_size)        
+        self.BatchNorm = nn.BatchNorm1d(num_features=embedding_size, momentum=0.05)
+
+    def forward(self, images):
+        features = self.custom_cnn(images)
+        features = self.BatchNorm(features)
+        return features
+
+#Paint2Code decoder
+class Decoder(nn.Module):
+
+    def __init__(self, embed_size, hidden_size, vocab_size, num_layers):
+        super(Decoder, self).__init__()
+
+        self.embed_size = embed_size
+        self.embed = nn.Embedding(num_embeddings=vocab_size, embedding_dim=embed_size)
+        self.lstm = nn.LSTM(input_size=embed_size, hidden_size=hidden_size, num_layers=num_layers, batch_first=True)
+        self.linear = nn.Linear(in_features=hidden_size, out_features=vocab_size)
+
+    def forward(self, features, captions, length):
+        embeddings = self.embed(captions)
+
+        features = features.unsqueeze(1)
+        embeddings = torch.cat((features, embeddings), 1)
+        packed = nn.utils.rnn.pack_padded_sequence(input=embeddings, lengths=length, batch_first=True)
+        hidden, _ = self.lstm(packed)
+        output = self.linear(hidden[0])
+
+        return output
+
+    def sample(self, features, states=None, longest_sentence_length=110):
+
+        sampled_ids = []
+        inputs = features.unsqueeze(1)
+
+        for i in range(longest_sentence_length):
+
+            hidden, states = self.lstm(inputs, states)
+            output = self.linear(hidden.squeeze(1))
+            predicted = output.max(dim=1, keepdim=True)[1]
+            sampled_ids.append(predicted)
+            inputs = self.embed(predicted)
+            inputs = inputs.view(-1, 1, self.embed_size)
+
+        sampled_ids = torch.cat(sampled_ids, 1)
+
+        return sampled_ids.squeeze()
---
+++ train.py
@@ -0,0 +1,114 @@
+import torch
+from torch.utils.data import DataLoader
+from pathlib import Path
+
+from vocab import Vocab
+from dataset import Paint2CodeDataset
+from utils import collate_fn, save_model, img_transformation
+from modelArchitecture.modelCustomCNN import Encoder, Decoder
+
+# Set parameters
+data_path = Path("data", "all_data")
+vocab_file_path = Path(data_path.parent, "vocab.txt")
+use_cuda = True  
+img_size = 224
+save_after_epochs = 1
+models_dir = Path("./modelsCustomCNN/models")
+batch_size = 4
+epochs = 100
+lr = 0.001
+seed = 42
+
+# Seed setup for reproducibility
+torch.manual_seed(seed)
+torch.cuda.manual_seed(seed)
+
+vocab = Vocab(vocab_file_path)
+assert len(vocab) > 0, "Vocabulary must be non-empty"
+
+print(vocab)
+
+# Setup GPU for training
+device = torch.device("cuda" if torch.cuda.is_available() and use_cuda else "cpu")
+print(f"Using device: {device}")
+
+# Define image transformation using the ResNet specs
+transform_imgs = img_transformation(img_size)
+
+train_loader = DataLoader(
+    Paint2CodeDataset(data_path, "train", vocab, transform=transform_imgs),
+    batch_size=batch_size,
+    collate_fn=lambda data: collate_fn(data, vocab=vocab),
+    pin_memory=True if use_cuda else False,
+    drop_last=True
+)
+print("Data loader for train created successfully.")
+
+val_loader = DataLoader(
+    Paint2CodeDataset(data_path, "validation", vocab, transform=transform_imgs),
+    batch_size=batch_size,
+    collate_fn=lambda data: collate_fn(data, vocab=vocab),
+    pin_memory=True if use_cuda else False,
+    drop_last=True
+)
+print("Data loader for val created successfully.")
+
+# Model parameters
+embed_size = 64
+hidden_size = 256
+num_layers = 2
+
+# Initialize the Encoder and Decoder
+encoder = Encoder(embed_size).to(device)
+decoder = Decoder(embed_size, hidden_size, len(vocab), num_layers).to(device)
+
+print("Models are initialized and moved to the designated device.")
+
+# Define loss function and optimizer
+criterion = torch.nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(list(encoder.parameters()) + list(decoder.parameters()), lr=lr)
+
+# Training and valing loops
+train_losses = []
+val_losses = []
+
+print("Starting training...")
+
+for epoch in range(epochs):
+    total_train_loss = num_batches = 0
+    encoder.train()
+    decoder.train()
+
+    for images, captions, lengths in train_loader:
+        images, captions = images.to(device), captions.to(device)
+        optimizer.zero_grad()
+        targets = torch.nn.utils.rnn.pack_padded_sequence(captions, lengths, batch_first=True)[0]
+        loss = criterion(decoder(encoder(images), captions, lengths), targets)
+        loss.backward()
+        optimizer.step()
+        total_train_loss += loss.item()
+        num_batches += 1
+
+    train_losses.append(total_train_loss / num_batches)
+
+    encoder.eval()
+    decoder.eval()
+    total_val_loss = num_val_batches = 0
+
+    with torch.no_grad():
+        for images, captions, lengths in val_loader:
+            images, captions = images.to(device), captions.to(device)
+            targets = torch.nn.utils.rnn.pack_padded_sequence(captions, lengths, batch_first=True)[0]
+            loss = criterion(decoder(encoder(images), captions, lengths), targets)
+            total_val_loss += loss.item()
+            num_val_batches += 1
+
+    val_losses.append(total_val_loss / num_val_batches)
+
+    print(f'Epoch {epoch}: Training Loss {train_losses[-1]:.4f}, val Loss {val_losses[-1]:.4f}')
+
+    if epoch != 0 and epoch % save_after_epochs == 0:
+        save_model(models_dir, encoder, decoder, optimizer, epoch, val_losses[-1], batch_size, vocab)
+        print(f"Saved model checkpoint at epoch: {epoch}")
+
+print("Training completed!")
